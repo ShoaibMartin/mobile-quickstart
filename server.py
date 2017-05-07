769b55c71029b1943220b3e7e06aee13ee6da1a7
ShoaibMartin <s.matin@pomtech.com>
2017-05-07 11:28:32 +0430

    Update server.py
    some changes

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ def call():
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
   resp = twilio.twiml.Response()
-  resp.say("Hi Shoaib Jan, This is Heroko Deploy Test")
+  resp.say("Hi Shoaib Jan, This is Heroko Deploy Test. some changes")
   return str(resp)
 
 if __name__ == "__main__":
